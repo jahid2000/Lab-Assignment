c8980f4774e80b60f336bbb10e5b22cee8ae7f39
jahid2000 <54980681+jahid2000@users.noreply.github.com>
2022-03-09 18:01:22 +0600

    modified index file

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send(`<h2> This is home page</h2> </br> </br>  <h3> Name : MD Jahidul Islam </br> Id : 1910176119/h3>`);
+    res.send(`<h2> This is home page</h2> </br> </br>  <h3> Name : MD Jahidul Islam </br> Id : 1910176119</h3>`);
 });
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
