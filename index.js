f9313195b90be2b602983b1b0c38b9063881ac73
jahid2000 <54980681+jahid2000@users.noreply.github.com>
2022-04-03 22:09:40 +0600

    finilize the index file

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const dishRouter = require('./routes/dishRouter');
 const promoRouter = require('./routes/promoRouter');
 const leaderRouter = require('./routes/leaderRouter');
 
 const app = express();
 const PORT = 3000;
+const url = 'mongodb://127.0.0.1:27017/assignment2';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+    console.log("Connected correctly to server");
+}, (err) => { console.log(err); });
 
 app.get('/', (req, res) => {
-    res.send(`<h2> This is home page</h2> </br> </br>  <h3> Name : MD Jahidul Islam </br> Id : 1910176119</h3>`);
+    res.send(`<h2> This is home page</h2> </br> </br>  <h3> Name : MD Jahidul Islam </br> Id : 1910176119/h3>`);
 });
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
